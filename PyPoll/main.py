65c70b19f068899a267db93b8a39136141410c47
Olga Petrova <mascee@gmail.com>
2024-08-19 12:26:44 -0700

    Commit python challenge August 19th

---
+++ PyPoll/main.py
@@ -0,0 +1,60 @@
+#You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+#The total number of votes cast
+# A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote
+
+import os
+import csv 
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+#Open and read csv
+with open(election_data_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    #Skip the header
+    csv_header = next(csv_reader)
+
+    #declare variables to work with data
+    total_votes = 0
+    candidates = {}
+    winner = ""
+    vote_count = 0
+
+    #Loop through each row in election data
+    for row in csv_reader:
+        #count total votes
+        total_votes += 1
+        #take candidate info from row 3
+        candidate = row[2]
+
+        #Add candidate to the dictionary and update vote count
+        if candidate in candidates:
+            candidates[candidate] += 1
+        else:
+            candidates[candidate] = 1
+
+#Calculate total votes
+analysis_txt = os.path.join("analysis", "PyPollAnalysis.txt")
+with open(analysis_txt, mode="w") as txt_file:
+    txt_file.write("Election Results\n")
+    txt_file.write("______________________________________\n")
+    txt_file.write(f"Total Votes: {total_votes}\n")
+    txt_file.write("______________________________________\n")
+
+
+    #Caclulate the percemtage of votes and find the winner
+    for candidate, votes in candidates.items():
+        vote_percentage = (votes/total_votes) *100
+        txt_file.write(f"{candidate}: {vote_percentage:.3f}% ({votes})\n")
+
+        if votes > vote_count:
+            vote_count = votes
+            winner = candidate
+
+    txt_file.write("________________________________________\n")
+    txt_file.write(f"Winner: {winner}\n")
+    txt_file.write("________________________________________\n")
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# python-challenge
-python-challenge
+Python challenge
+Olga Petrova
+
+2 tasks are performed.
+
+First program in the folder PyBank reads budget_data.txt in the Resources folder and analyses data in it.
+The results are outputted in the PyBankAnalysis.txt in the analysis folder.
+Here is the output just in case:
+Financial Analysis: 
+_________________________________
+Total Month: 86
+Total: $22564198
+Average change: $-8311.105882352942
+Greatest Increase in Profits: Aug-16 ($1862002)
+Greatest Decrease in Profits: Feb-14 ($-1825558)
+
+
+The python code is in the main.py 
+
+
+Second program in the folder PyPoll reads election data from election_data.txt in the Resources folder and calculates election results.
+The results are outputted in the PyPollAnalysis.txt
+
+Here is the output just in case:
+Election Results
+______________________________________
+Total Votes: 369711
+______________________________________
+Charles Casper Stockham: 23.049% (85213)
+Diana DeGette: 73.812% (272892)
+Raymon Anthony Doane: 3.139% (11606)
+________________________________________
+Winner: Diana DeGette
+________________________________________
+
